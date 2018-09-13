9fdcc869e03391eafbab3af433290b00c1a518e4
tuananh <tuananh@gmail.com>
2018-09-13 09:46:18 +0700

    create thread login

--- app/src/main/java/com/example/tuananh/hotel/MainActivity.java
+++ app/src/main/java/com/example/tuananh/hotel/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.tuananh.hotel;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_login;
+    RelativeLayout layoutsplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutsplash = findViewById(R.id.layoutdemo);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (layoutsplash!=null)
+                            layoutsplash.setVisibility(View.GONE);
+
+                    }
+                });
+            }
+
+
+        }).start();
 
         btn_login = (Button)findViewById(R.id.btnlogin);
         btn_login.setOnClickListener(new View.OnClickListener() {
